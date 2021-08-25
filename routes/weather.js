7532c2049e89045cbbdc0dc47ad573f4343cb9e9
Antonina Vracinska <avracinska@globalblue.com>
2021-08-25 11:17:29 +0200

    apiKey

--- routes/weather.js
+++ routes/weather.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var apiKey = "9bd3d07aab4e665efd43c50664dc9c7c";
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('response from other api');
