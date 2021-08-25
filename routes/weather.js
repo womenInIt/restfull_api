9cc7531d0a16c4319cbf832eab69851198047f19
Antonina Vracinska <avracinska@globalblue.com>
2021-08-25 11:45:28 +0200

    fetch

--- routes/weather.js
+++ routes/weather.js
@@ -1,6 +1,8 @@
 var express = require('express');
+const fetch = require("node-fetch");
 var router = express.Router();
 
+var baseWeatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 var apiKey = "9bd3d07aab4e665efd43c50664dc9c7c";
 
 /* GET users listing. */
