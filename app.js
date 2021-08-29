4132f90eefaf5aa8a813592cfebd7d21093d52ba
fialo4ka <tona16@inbox.lv>
2021-08-29 21:01:57 +0200

    working swagger

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const path = require('path');
 const logger = require('morgan');
 const expressOasGenerator = require('express-oas-generator');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const indexRouter = require('./routes/ideas');
 const weatherRouter = require('./routes/weather');
 
 var app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,20 +22,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/weather', weatherRouter);
 
-expressOasGenerator.handleResponses(app, {});
-expressOasGenerator.handleRequests();
+// expressOasGenerator.handleResponses(app, {});
+// expressOasGenerator.handleRequests();
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.sendStatus(err.status || 500);
-});
+//   res.sendStatus(err.status || 500);
+// });
 
 module.exports = app;
